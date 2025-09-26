04a9c45d1827a9061925dc72d87f56589cc7dbc9
Yaqub <perpetualdemise@gmail.com>
2025-09-26 17:06:57 +0200

    thnx

--- page-object/datePicker.ts
+++ page-object/datePicker.ts
@@ -7,10 +7,24 @@ export class datePickerPage{
         }
 
         async selectCommonDatepickerDateFromToday(numberOfDaysFromToday: number){
-            let calendarInputField = this.page.locator('nb-card-body').getByPlaceholder('Form Picker')
+            let calendarInputField = this.page.getByPlaceholder('Form Picker')
             await calendarInputField.click()
+            let dateToAssert = await this.selectDateInTheCalendar(numberOfDaysFromToday)
+            await expect(calendarInputField).toHaveValue(dateToAssert)    
+        }
+
+        async datePickerWithDateRangeFromToday(startDateFromToday : number, endDayFromToday : number){
+            let calendarInputField = this.page.locator('nb-card-body').getByPlaceholder('Range Picker')
+            await calendarInputField.click()
+            let dateToAssertStart = await this.selectDateInTheCalendar(startDateFromToday)
+            let dateToAssertEnd = await this.selectDateInTheCalendar(endDayFromToday)
+            let dateToAssert = `${dateToAssertStart} - ${dateToAssertEnd}`
+            await expect(calendarInputField).toHaveValue(dateToAssert) 
+        }
+
+        private async selectDateInTheCalendar(numberOfDaysFromToday: number){
             let date = new Date()
-            date.setDate(date.getDate() + 28)
+            date.setDate(date.getDate() + numberOfDaysFromToday)
             let expectedDate = date.getDate().toString()
             let expectedMonthShort = date.toLocaleString('En-US', {month: 'short'})
             let expectedMonthLong = date.toLocaleString('En-US', {month: 'long'})
@@ -24,7 +38,7 @@ export class datePickerPage{
                 calendarMonthAndYear = await this.page.locator('nb-calendar-view-mode').textContent()
         }
         
-            await this.page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate, {exact : true}).click()
-            await expect(calendarInputField).toHaveValue(dateToAssert)    
+            await this.page.locator('.day-cell.ng-star-inserted').getByText(expectedDate, {exact : true}).click()
+            return dateToAssert
         }
 }
